4dcb50b8ec0ac79f5df98697516a6a5427ef7998
David Alves <david27alves@hotmail.com>
2019-01-23 15:15:47 -0200

    Update controllers.js

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,6 +1,6 @@
 app.controller('mlabCtrl', ['$scope', '$http', function($scope, $http)
 {
-   var $scope.versoes;
+   var versoes;
    var baseurl = 'https://api.mlab.com/api/1/databases/vrversions/collections/versions?apiKey=gkPeeEULfjRhBbL51CSPsI80eW9dMOtZ';
    
    $http.get(baseurl).then(function(response) {
