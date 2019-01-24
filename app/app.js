b14f5508af91b66dcfd119a4252e230d87b9f3ba
David Alves <david27alves@hotmail.com>
2019-01-24 09:53:55 -0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,8 @@ app.config(function($routeProvider, $locationProvider)
 {
 
    $routeProvider.when('/', {
-      "templateUrl" : "app/views/home.html"
+      templateUrl: "app/views/home.html",
+	controller: "mlabCtrl"
    }).when('/cadastro', {
 		"templateUrl": "app/views/cadastro.html"
 	})
